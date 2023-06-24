52acec2f53dfa97b609ae9b9177f0d0439beeb33
JuanCarlos-27 <jcromero909@misena.edu.co>
2023-06-24 12:12:02 -0500

    Prepare blocked and checked step

--- src/components/ProgressBar.jsx
+++ src/components/ProgressBar.jsx
@@ -4,10 +4,10 @@ export default function ProgressBar () {
   return (
     <section className='progress-bar'>
       <Step stepLabel='Paso 1' />
-      <Step stepLabel='Paso 2' />
-      <Step stepLabel='Paso 3' />
-      <Step stepLabel='Paso 4' />
-      <Step stepLabel='Paso 5' />
+      <Step stepLabel='Paso 2' isCurrentStep />
+      <Step stepLabel='Paso 3' isBlockedStep />
+      <Step stepLabel='Paso 4' isBlockedStep />
+      <Step stepLabel='Paso 5' isBlockedStep />
     </section>
   );
 }
